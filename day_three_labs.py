f2e1d5c695ab2fe7be370072407126fc798c700e
Gregory Green <gcg145@gmail.com>
2020-07-22 12:53:42 -0700

    day three labs

---
+++ day_three_labs.py
@@ -0,0 +1,34 @@
+# Create two variables a, b that's set equal to some numbers.
+
+a = 5
+b = 10
+
+print( a + b )
+print( a * b )
+print( a - b )
+print( a / b )
+print( a % b )
+
+print()
+# Create a string x and set it equal to 'python'.
+
+x = "python"
+
+print(x[0])
+print(x[-1])
+print(len(x))
+print(x.upper())
+
+print()
+
+# Create a list called nums that by default is empty.
+
+nums = []
+
+nums.append(10)
+nums.append(100)
+nums.append(10)
+nums.append(500)
+nums.append(56)
+nums.pop()
+print(nums)
