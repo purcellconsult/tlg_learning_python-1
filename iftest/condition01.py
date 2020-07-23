5116965339bbc1af9db43b4c020f11bf8a671375
Gregory Green <gcg145@gmail.com>
2020-07-23 10:47:13 -0700

    iftest documents

---
+++ iftest/condition01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+hostname = "MTG"
+if hostname == "MTG":
+	print("the hostname was found to be mtg")
---
+++ iftest/condition02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+hostname = input("What value should we set for hostname?" )
+
+if hostname == "MTG":
+    print("The host was found to be mtg")
